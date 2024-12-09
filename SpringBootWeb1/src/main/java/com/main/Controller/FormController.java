3e603f0a8d1aac53192f55465584c344b31b8826
Ganesh_Kulkarni <ganeshkulkarni4237@gmail.com>
2024-12-09 10:20:49 +0530

    Worked on data pasing from one page to another page

--- SpringBootWeb1/src/main/java/com/main/Controller/FormController.java
+++ SpringBootWeb1/src/main/java/com/main/Controller/FormController.java
@@ -3,6 +3,9 @@ package com.main.Controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+
 @Controller
 public class FormController {
 
@@ -12,4 +15,19 @@ public class FormController {
 		System.out.println("FormController.openForm()");
 		return "form.jsp";
 	}
+	
+	@RequestMapping("add")
+	public String add(HttpServletRequest req,HttpSession session) // using servlet
+	{
+		int num1=Integer.parseInt(req.getParameter("num1"));
+		int num2=Integer.parseInt(req.getParameter("num2"));
+		int result=num1+num2;
+		
+		session.setAttribute("result",result);
+		return "result.jsp";
+	}
+	
+
+	
+	
 }
