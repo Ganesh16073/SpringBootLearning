83126b4bac995020efbd0cc00c22df57bf0ac7dd
Ganesh_Kulkarni <ganeshkulkarni4237@gmail.com>
2025-01-13 16:01:34 +0530

    implemented Language entity CRUDE Service

--- SpringBoot-MovieBookingApplication/src/main/java/com/main/service/ILanguageService.java
+++ SpringBoot-MovieBookingApplication/src/main/java/com/main/service/ILanguageService.java
@@ -1,11 +1,19 @@
 package com.main.service;
 
+import java.util.Optional;
+
 import com.main.model.Language;
 
 public interface ILanguageService {
 	public String addLanguage(Language language) throws Exception;
 	public boolean isLanguageAvaliableById(Integer id) throws Exception;
 	public Long getLanguageCount()throws Exception;
+	public Iterable<Language> getAllLanguage() throws Exception;
+	public Iterable<Language> getAllLanguageByIds(Iterable<Integer> ids)throws Exception;
+	public Optional<Language> getLanguageById(Integer id)throws Exception;
+	public Language getLanguageByIdOrGetDefaultLanguage(Integer id)throws Exception;
+	public String updateLanguageNameById(Integer id, String updateLangName)throws Exception;
+	public String removeLanguageById(Integer id)throws Exception;
 	
 
 }
--- SpringBoot-MovieBookingApplication/src/main/java/com/main/service/LanguageServiceImp.java
+++ SpringBoot-MovieBookingApplication/src/main/java/com/main/service/LanguageServiceImp.java
@@ -15,7 +15,7 @@ public class LanguageServiceImp implements ILanguageService{
 	private ILanguageRepository langRepo;
 
 	@Override
-	public String addLanguage(Language language) throws Exception {
+	public String addLanguage(Language language)throws Exception {
 		try
 		{
 			Language lan=langRepo.save(language);
@@ -25,7 +25,6 @@ public class LanguageServiceImp implements ILanguageService{
 			throw e;
 		}
 	}
-
 	@Override
 	public boolean isLanguageAvaliableById(Integer id) throws Exception {
 		try
@@ -43,13 +42,96 @@ public class LanguageServiceImp implements ILanguageService{
 			throw e;
 		}
 	}
-
 	@Override
 	public Long getLanguageCount() throws Exception {
 		try
 		{
 			return langRepo.count();
 			
+		}catch (Exception e) {
+			throw e;
+		}
+	}
+
+	@Override
+	public Iterable<Language> getAllLanguage() throws Exception {
+		try
+		{
+			return langRepo.findAll();
+		}catch (Exception e) {
+			throw e;
+		}
+		
+	}
+	@Override
+	public Iterable<Language> getAllLanguageByIds(Iterable<Integer> ids) throws Exception {
+		try
+		{
+			return langRepo.findAllById(ids);
+		}catch (Exception e) {
+			throw e;
+		}
+		
+	}
+	@Override
+	public Optional<Language> getLanguageById(Integer id) throws Exception {
+		try
+		{
+			return langRepo.findById(id);
+		}catch (Exception e) {
+			throw e;
+		}
+		
+	}
+	@Override
+	public Language getLanguageByIdOrGetDefaultLanguage(Integer id) throws Exception {
+		try
+		{
+			Optional<Language> lan=langRepo.findById(id);
+			if(lan.isPresent())
+			{
+				return lan.get();
+			}
+		}catch (Exception e) {
+			throw e;
+		}
+		return new Language(0,"Default");
+	}
+	@Override
+	public String updateLanguageNameById(Integer id, String updateLangName) throws Exception {
+		try
+		{
+			Optional<Language> lang=langRepo.findById(id);
+			if(lang.isPresent())
+			{
+				lang.get().setLanguageName(updateLangName);
+				langRepo.save(lang.get());
+				return "The Language is Updated";
+				
+			}
+			else
+			{
+				return "The Language id : "+id+" is Not Found";
+			}
+			
+		}catch (Exception e) {
+			throw e;
+		}
+	}
+	@Override
+	public String removeLanguageById(Integer id) throws Exception {
+		try
+		{
+			Optional<Language> lang=langRepo.findById(id);
+			if(lang.isPresent())
+			{
+				langRepo.deleteById(id);
+				return id+" : id Language is Deleted";
+			}
+			else
+			{
+				return "The Language is not found with id "+id;
+			}
 		}catch (Exception e) {
 			throw e;
 		}
